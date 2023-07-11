824070a5f48dd275b900c1ad4e48a0b2cc03f2b8
KodingCore <kcorvais@gmail.com>
2023-07-11 14:56:27 +0000

    router

---
+++ logics/router.php
@@ -0,0 +1,29 @@
+<?php
+
+function checkRoute(string $route) : void
+{
+    if($route === "categories")
+    {
+        
+    }
+    else if($route === "room")
+    {
+        
+    }
+    else if($route === "user-register")
+    {
+        
+    }
+    else if($route === "user-login")
+    {
+        
+    }
+    else if($route === "user-account")
+    {
+        
+    }
+    else if($route === "")
+    {
+        
+    }
+}
