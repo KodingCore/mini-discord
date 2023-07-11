75aacb184770a7da0f40562657b8fba236cd560e
guillaumeTesier <guillaumetessierpro@gmail.com>
2023-07-11 14:22:02 +0000

    table message.php

--- models/Message.php
+++ models/Message.php
@@ -3,13 +3,15 @@
 class Message extends AbstractModel
 {
     private string $content;
+    private string $creation_date;
     private int $user_id;
     private int $room_id;
 
-    public function __construct(string $content, int $user_id, int $room_id)
+    public function __construct(string $content,string $creation_date, int $user_id, int $room_id)
     {
         AbstractModel::__construct();
         $this->content = $content;
+        $this->creation_date = $creation_date;
         $this->user_id = $user_id;
         $this->room_id = $room_id;
     }
@@ -17,6 +19,11 @@ class Message extends AbstractModel
     public function getContent(): string
     {
         return $this->content;
+    }
+    
+    public function getCreation_date(): string
+    {
+        return $this->creation_date;
     }
 
     public function getUser_id(): int
@@ -33,6 +40,11 @@ class Message extends AbstractModel
     public function setContent(string $content): void
     {
         $this->content = $content;
+    }
+    
+    public function setCreation_date(string $creation_date):void
+    {
+        $this->content = $creation_date;
     }
 
     public function setUser_id(int $user_id): void
