8039d5efd069c556ce6be7f7a06b8d9f14d8381f
3lvir4 <pierreavril18@gmail.com>
2023-07-11 15:27:15 +0200

    reformat messageManager

--- managers/MessageManager.php
+++ managers/MessageManager.php
@@ -1,39 +1,41 @@
 <?php
 
- require 'AbstractManager.php';
- 
- class MessageManager extends AbstractManager{
-     
-     
-     function addMessage(Message $message) : void
-     {
-         $query =$this->db->prepare('INSERT INTO messages(content,user_id,room_id) VALUES(:content,:user_id,:room_id)');
-         $parameters=[
-             'content' => $message->getContent(),
-             'user_id' => $message->getUser_id(),
-             'room_id' => $message->getRoom_id()
-        ];
-         $query->execute($parameters);
-     }
-     
-     public function editMessage(Message $message) :void {
-      
+require 'AbstractManager.php';
+
+class MessageManager extends AbstractManager
+{
+
+
+   function addMessage(Message $message): void
+   {
+      $query = $this->db->prepare('INSERT INTO messages(content,user_id,room_id) VALUES(:content,:user_id,:room_id)');
+      $parameters = [
+         'content' => $message->getContent(),
+         'user_id' => $message->getUser_id(),
+         'room_id' => $message->getRoom_id()
+      ];
+      $query->execute($parameters);
+   }
+
+   public function editMessage(Message $message): void
+   {
+
       $query = $this->db->prepare('
          UPDATE message
          SET content = :content,
          WHERE id = :id
       
       ');
-      
+
       $parameters = [
-       
-       'content' => $message->getContent(),
-       'id' => $message->getId()
-		];
-		    $query->execute($parameters);
-		    
-    }
-    public function deleteMessage(Message $message) : void {
+
+         'content' => $message->getContent(),
+         'id' => $message->getId()
+      ];
+      $query->execute($parameters);
+   }
+   public function deleteMessage(Message $message): void
+   {
       $query = $this->db->prepare('
          DELETE FROM messages
          WHERE messages.id = :id
@@ -43,9 +45,4 @@
       ];
       $query->execute($parameters);
    }
-     
- }
-
-
-
-?>
+}
