3b9fa2bab3f697e3c54e1350036b4f7c7e1f528b
3lvir4 <pierreavril18@gmail.com>
2023-07-11 15:48:50 +0200

    quick reformat

--- models/Category.php
+++ models/Category.php
@@ -1,25 +1,23 @@
 <?php
- 
- class Category extends AbstractModel {
-     
-      private string $name;
-     
-      public function __construct(string $name) {
+
+class Category extends AbstractModel
+{
+
+    private string $name;
+
+    public function __construct(string $name)
+    {
         AbstractModel::__construct();
-        $this->name=$name;
-          
-      }
-     
-      public function  getName() : string
-     {
-         return $this->name;
-     }
-     
-      public function  setName(string $name)
-     {
-         return $this->name=$name;
-     }
-     
-     
-     
- }
+        $this->name = $name;
+    }
+
+    public function  getName(): string
+    {
+        return $this->name;
+    }
+
+    public function  setName(string $name)
+    {
+        return $this->name = $name;
+    }
+}
--- models/Message.php
+++ models/Message.php
@@ -6,43 +6,41 @@ class Message extends AbstractModel
     private int $user_id;
     private int $room_id;
 
-    public function __construct(string $content , int $user_id , int $room_id)
+    public function __construct(string $content, int $user_id, int $room_id)
     {
         AbstractModel::__construct();
         $this->content = $content;
         $this->user_id = $user_id;
         $this->room_id = $room_id;
-        
-        
     }
 
-    public function getContent() : string
+    public function getContent(): string
     {
         return $this->content;
     }
-    
-    public function getUser_id() : int
+
+    public function getUser_id(): int
     {
         return $this->user_id;
     }
-    
-    public function getRoom_id() : int
+
+    public function getRoom_id(): int
     {
         return $this->room_id;
     }
 
 
-    public function setContent(string $content) : void
+    public function setContent(string $content): void
     {
         $this->content = $content;
     }
-    
-     public function setUser_id(int $user_id) : void
+
+    public function setUser_id(int $user_id): void
     {
         $this->user_id = $user_id;
     }
-    
-     public function setRoom_id(int $room_id) : void
+
+    public function setRoom_id(int $room_id): void
     {
         $this->room_id = $room_id;
     }
