aa56a0e5c3be0e96ea8d49ebf72a79bade00d028
guillaumeTesier <guillaumetessierpro@gmail.com>
2023-07-11 11:33:39 +0000

    ajout class messages

--- models/Message.php
+++ models/Message.php
@@ -6,7 +6,7 @@ class Message extends AbstractModel
     private int $user_id;
     private int $room_id;
 
-    public function __construct(string $content)
+    public function __construct(string $content , int $user_id , int $room_id)
     {
         AbstractModel::__construct();
         $this->content = $content;
@@ -20,9 +20,30 @@ class Message extends AbstractModel
     {
         return $this->content;
     }
+    
+    public function getUser_id() : int
+    {
+        return $this->user_id;
+    }
+    
+    public function getRoom_id() : int
+    {
+        return $this->room_id;
+    }
+
 
     public function setContent(string $content) : void
     {
         $this->content = $content;
+    }
+    
+     public function setUser_id(int $user_id) : void
+    {
+        $this->user_id = $user_id;
+    }
+    
+     public function setRoom_id(int $room_id) : void
+    {
+        $this->room_id = $room_id;
     }
 }
