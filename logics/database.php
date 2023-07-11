0511db46ed2f9df42dc31a34f18a30609b329235
KodingCore <131252253+KodingCore@users.noreply.github.com>
2023-07-11 11:49:15 +0200

    database completion

--- logics/database.php
+++ logics/database.php
@@ -1,12 +1,12 @@
 <?php
 
-const DATABASE_NAME = ;
-const DATABSE_PORT = ;
-const DATABASE_HOST = ;
-const DATABASE_USERNAME = ;
-const DATABASE_PASSWORD = ;
+const DATABASE_NAME = "kevincorvaisier_mini-discord";
+const DATABASE_PORT = "3306";
+const DATABASE_HOST = "db.3wa.io";
+const DATABASE_USERNAME = "kevincorvaisier";
+const DATABASE_PASSWORD = "04646b679a4ab0a202f8007ea81fe675";
 
-$connexionString = 'mysql:host='.DATABSE_HOST.';port='.DATABASE_PORT.';dbname='.DATABASE_NAME;
+$connexionString = 'mysql:host='.DATABASE_HOST.';port='.DATABASE_PORT.';dbname='.DATABASE_NAME;
 $db = new PDO(
    $connexionString,
    DATABASE_USERNAME,
