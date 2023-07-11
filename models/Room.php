645ce3ed0684005475910ff37e0ffeca51ef4855
KodingCore <kcorvais@gmail.com>
2023-07-11 13:32:29 +0000

    Room.php

--- models/Room.php
+++ models/Room.php
@@ -2,11 +2,13 @@
 class Room extends AbstractModel
 {
     private string $name;
+    private int $category_id;
 
-    public function __construct(string $name)
+    public function __construct(string $name, int $category_id)
     {
         AbstractModel::__construct();
         $this->name = $name;
+        $this->category_id = $category_id;
     }
 
     public function getName() : string
@@ -17,5 +19,15 @@ class Room extends AbstractModel
     public function setName(string $name)
     {
         $this->name = $name;
+    }
+    
+    public function getCategory_id() : int
+    {
+        return $this->category_id;
+    }
+
+    public function setCategory_id(int $id)
+    {
+        $this->category_id = $id;
     }
 }
