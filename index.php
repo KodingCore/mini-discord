3950c707b7b38006552a1216865306e086968778
KodingCore <kcorvais@gmail.com>
2023-07-12 13:19:13 +0000

    init index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'logics/database.php';
+require_once 'logics/router.php';
+
+require_once 'models/Category.php';
+require_once 'models/Message.php';
+require_once 'models/Room.php';
+require_once 'models/User.php';
+
+require_once 'managers/CategoryManager.php';
+require_once 'managers/MessageManager.php';
+require_once 'managers/RoomManager.php';
+require_once 'managers/UserManager.php';
+
+require_once 'controllers/CategoriesController.php';
+require_once 'controllers/ChatController.php';
+require_once 'controllers/HomeController.php';
+require_once 'controllers/UserController.php';
+
